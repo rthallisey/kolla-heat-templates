b8a3c1f10c602bb95347129e87141d696c5e605d
Ryan Hallisey <rhallise@redhat.com>
2016-03-23 15:50:20 +0000

    We don't need to use 'sudo' in firstboot
    Commands run in Cloudinit don't need sudo.

--- firstboot/start_docker.sh
+++ firstboot/start_docker.sh
@@ -4,7 +4,7 @@ set -eux
 /sbin/setenforce 0
 /sbin/modprobe ebtables
 
-cat | sudo tee /etc/yum.repos.d/docker.repo << EOF
+cat | tee /etc/yum.repos.d/docker.repo << EOF
 [docker]
 name=Docker Main Repository
 baseurl=https://yum.dockerproject.org/repo/main/fedora/23/
@@ -13,14 +13,14 @@ gpgcheck=1
 gpgkey=https://yum.dockerproject.org/gpg
 EOF
 
-sudo dnf install -y libffi-devel openssl-devel docker-engine btrfs-progs python-docker-py
+dnf install -y libffi-devel openssl-devel docker-engine btrfs-progs python-docker-py
 
-sudo sed -i -r 's,(ExecStart)=(.+),\1=/usr/bin/docker daemon --insecure-registry=$docker_registry,' /usr/lib/systemd/system/docker.service
-sudo sed -i 's|^MountFlags=.*|MountFlags=shared|' /usr/lib/systemd/system/docker.service
-sudo systemctl daemon-reload
+sed -i -r 's,(ExecStart)=(.+),\1=/usr/bin/docker daemon --insecure-registry=$docker_registry,' /usr/lib/systemd/system/docker.service
+sed -i 's|^MountFlags=.*|MountFlags=shared|' /usr/lib/systemd/system/docker.service
+systemctl daemon-reload
 
-sudo systemctl start docker
-sudo docker info
+systemctl start docker
+docker info
 
 # Hostname needs to resolve
 cat <<EOF >> /etc/hosts
