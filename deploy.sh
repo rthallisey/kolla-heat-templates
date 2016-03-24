8226064dcb6693cd7d6474184ebeef547194ceb7
Ian Main <imain@redhat.com>
2016-03-24 19:32:43 +0000

    Use tee.

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,4 @@
 
 openstack overcloud deploy --templates=kolla-heat-templates -e kolla-heat-templates/environments/kolla.yaml
 
-heat output-show overcloud ip_addresses
-heat output-show overcloud ip_addresses > overcloud_ip_list.txt
+heat output-show overcloud ip_addresses | tee overcloud_ip_list.txt
